708d7cd56050d1c4b461b937a738d165e899d65e
Darius Baciu <baciu.darius01@gmail.com>
2024-05-30 09:25:51 +0300

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
         <Button label='Shop now' iconURL={arrowRight} />
 
-        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-[2.75rem]'>
           {statistics.map((stat, index) => (
             <div key={index}>
               <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
@@ -45,16 +45,16 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen rounded-lg max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImg}
           alt='shoe colletion'
-          width={610}
-          height={502}
+          width={400}
+          height={400}
           className='object-contain relative z-10'
         />
 
-        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[8%] sm:left-[10%] max-sm:px-6 xl:bottom-[4%]'>
           {shoes.map((image, index) => (
             <div key={index}>
               <ShoeCard
